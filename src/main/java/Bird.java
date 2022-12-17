550e4c36dc1d352ccd215cc571ed3327bc871832
zwl <zwl@qq.com>
2022-12-17 20:18:23 +0800

    idea commit 4

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -3,6 +3,6 @@ public class Bird {
         System.out.println("你好");
         System.out.println("hot-fix你好");
         System.out.println("我是一只鸟");
-        System.out.println("我是在github中修改的")
+        System.out.println("我是在github中修改的");
     }
 }
