c0787cb43383d3eb6e0176679007448bafbc4276
zhouweilin6 <119643194+zhouweilin6@users.noreply.github.com>
2023-01-25 19:03:00 +0800

    Update Dog.java
    这是在github中修改的！

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -2,5 +2,6 @@ public class Dog {
     public static void main(String[] args) {
         System.out.println("我是个狗！");
         System.out.println("我修改了，我是一条狗");
+        System.out.println("我在github中修改了，我是在github的一条狗");
     }
 }
